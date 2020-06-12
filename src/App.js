d03bf75de62e17a3a24115e710ffb8e9cd8cec6a
oscarzamora <oscarnewsletter1@gmail.com>
2020-06-12 00:31:23 -0500

    borrando componentes de prueba

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import HolaMundo from "./components/HolaMundo";
-import AdiosMundo from "./components/AdiosMundo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <HolaMundo />
-        <AdiosMundo />
       </header>
     </div>
   );
