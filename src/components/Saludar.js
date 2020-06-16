885de5f3fb667ddec33a4985e7c1eedd957cbba0
oscarzamora <oscarnewsletter1@gmail.com>
2020-06-16 17:18:50 -0500

    el uso de la asignacion por destructuring

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 export default function Saludar(props) {
+  const { userInfo, saludarFn } = props;
+  const { nombre } = userInfo;
   console.log(props);
+  console.log(userInfo);
 
   return (
     <div>
-      <button onClick={() => props.saludarFn(props.userInfo.nombre)}>
-        Saludar
-      </button>
+      <button onClick={() => saludarFn(nombre)}>Saludar</button>
     </div>
   );
 }
